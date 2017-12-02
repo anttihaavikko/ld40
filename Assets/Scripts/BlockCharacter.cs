b1597b07d36561a4e70928143462e2508d4d1865
Antti Haavikko <antti.haavikko@focus.fi>
2017-12-02 22:37:47 +0200

    Add hats

--- Assets/Scripts/BlockCharacter.cs
+++ Assets/Scripts/BlockCharacter.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class BlockCharacter : MonoBehaviour {
 
 	private Animator anim;
+	public GameObject[] accessories;
+
+	public bool isPlayer = false;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		RandomizeSpeed ();
+
+		if (isPlayer) {
+			ActivatePreviousAccessories ();
+		} else {
+			ActivateCurrentAccessory ();
+		}
 	}
 	
 	// Update is called once per frame
@@ -21,5 +30,19 @@ public class BlockCharacter : MonoBehaviour {
 
 	void RandomizeSpeed() {
 		anim.speed = Random.Range (0.8f, 1.2f);
+	}
+
+	public void ActivateCurrentAccessory() {
+		if (ProgressManager.Instance.level < accessories.Length) {
+			accessories [ProgressManager.Instance.level].SetActive (true);
+		}
+	}
+
+	public void ActivatePreviousAccessories() {
+		for(int i = 0; i < ProgressManager.Instance.level; i++) {
+			if (i < accessories.Length) {
+				accessories [i].SetActive (true);
+			}
+		}
 	}
 }
